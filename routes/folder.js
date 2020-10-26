755e90f5aeedef63d7150a205e2cf172f3f13455
Bapubaby <nimumehta83@gmail.com>
2020-10-26 20:36:22 +0530

    changed

--- routes/folder.js
+++ routes/folder.js
@@ -39,6 +39,16 @@ router.get("/", (req, res) => {
     Folder.find().then(folders => res.status(200).json(folders)).catch(err => err)
 })
 
+router.post ("/files", (req, res) => {
+    Folder.findOne({name: req.body.folder}).then(folder => res.status(200).json(folder)).catch(err => err)
+})
+
+router.post("/files/download", (req, res) => {
+    console.log(req.body.path)
+    // res.download('./public/folders/nimit18/dentistry-1603369564083.pdf');
+    return res.sendFile(`C:/Users/nimum/OneDrive/Desktop/bene-backend-next/public/folders/${req.body.folder}/${req.body.file}`)
+})
+
 router.post("/add", (req, res ) => {
     if (!fs.existsSync(`./public/folders/${req.body.folder}`)){
         fs.mkdirSync(`./public/folders/${req.body.folder}`);
@@ -47,9 +57,11 @@ router.post("/add", (req, res ) => {
     folder.save().then(data => res.status(200).json(folder)).catch(err => err)
 })
 
-router.post("/:id/files/add", upload.any(), async(req, res) => {
+router.post("/files/add", upload.any(), async(req, res) => {
+    console.log(req)
     let folder, files
-    folder = await Folder.findOne({_id: req.params.id})
+    folder = await Folder.findOne({_id: req.body.id})
+    console.log(folder)
     if(folder){
         files = [...folder.files]
         req.files.map( (item, index) => {
@@ -75,15 +87,16 @@ router.post("/:id/files/add", upload.any(), async(req, res) => {
     folder.save().then(data => res.status(200).json(folder)).catch(err => err)     
 })
 
-router.post("/delete/:id", (req, res) => {
-    Folder.deleteOne({_id: req.params.id}).then(async () => {
-        await fs.remove(`./public/folders/${req.body.folder}`)
+router.post("/delete", async (req, res) => {
+    const folder = await Folder.findOne({_id: req.body.id})
+    Folder.deleteOne({_id: req.body.id}).then(async () => {
+        await fs.remove(`./public/folders/${folder.name}`)
         return res.status(200).json({message: 'Folder Deleted Successfully'})
     }).catch(err => err)
 })
 
-router.post("/edit/:id", async(req, res) => {
-    let folder = await Folder.findById(req.params.id)
+router.post("/edit", async(req, res) => {
+    let folder = await Folder.findById(req.body.id)
     console.log(folder)
     if(folder){
         fs.renameSync(`./public/folders/${folder.name}`, `./public/folders/${req.body.folder}`)
@@ -100,27 +113,28 @@ router.post("/edit/:id", async(req, res) => {
     }
 })
 
-router.post("/:id/files/delete/:fileId", async (req, res) => {
-    let folder = await Folder.findOne({_id: req.params.id})
+router.post("/files/delete", async (req, res) => {
+    let folder = await Folder.findOne({_id: req.body.id})
     let files = [...folder.files]
-    let file = files.filter(file => file._id == req.params.fileId)[0]
+    let file = files.filter(file => file._id == req.body.fileId)[0]
     console.log(file)
-    files = files.filter(file => file._id != req.params.fileId)
+    files = files.filter(file => file._id != req.body.fileId)
     folder.files = files
     fs.removeSync(`./public/folders/${folder.name}/${file.name}`)
     folder.save().then(data => res.status(200).json(folder)).catch(err => err)
 })
 
-router.post("/:id/files/edit/:fileId", async(req, res) => {
-    let folder = await Folder.findOne({_id: req.params.id})
+router.post("/files/edit", async(req, res) => {
+    let folder = await Folder.findOne({_id: req.body.id})
     console.log(folder.files)
     let files = [...folder.files]
-    let file = files.filter(file => file._id == req.params.fileId)[0]
+    let file = files.filter(file => file._id == req.body.fileId)[0]
     console.log(file)
     fs.renameSync(`./public/folders/${folder.name}/${file.name}`, `./public/folders/${folder.name}/${req.body.fileName}`)
     files = []
     folder.files.map(item => {
         if(item._id === file._id){
+            item.name = req.body.fileName
             item.path = `./public/folders/${folder.name}/${req.body.fileName}`
         }
         files.push(item)
