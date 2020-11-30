99fb79b9a229546c419af55788009f6da089ecd6
Bapubaby <nimumehta83@gmail.com>
2020-11-30 13:53:17 +0530

    subscribe data added

---
+++ routes/subscriberData.js
@@ -0,0 +1,23 @@
+const express = require("express");
+const router = express.Router();
+const DB = require("../config/database");
+const SubscriberData = DB.SubscriberData;
+
+router.get("/get", (req, res) => {
+  SubscriberData.find().then((result) => res.status(200).json(result));
+});
+
+router.post("/add", (req, res) => {
+  let subscriberData = new SubscriberData(req.body);
+  SubscriberData.findOne({ email: req.body.email }).then((result) => {
+    if (result) {
+      return res
+        .status(200)
+        .json({ message: "Email has already been subscribed" });
+    } else {
+      subscriberData.save().then((result) => res.status(200).json(result));
+    }
+  });
+});
+
+module.exports = router;
