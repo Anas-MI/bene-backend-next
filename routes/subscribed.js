915f7491af58588d3b581933880930c16c655260
bapubaby27 <nimumehta83@gmail.com>
2021-01-14 23:10:25 +0530

    extension added

--- routes/subscribed.js
+++ routes/subscribed.js
@@ -1,127 +1,129 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const DB = require('../config/database');
+const DB = require("../config/database");
 const Subscribed = DB.Subscribed;
-const OrderedProduct = require('../models/orderedProduct')
-const OOrder = require("../models/order")
+const OrderedProduct = require("../models/orderedProduct");
+const OOrder = require("../models/order");
 const name = "3BgRr2498";
-const transactionKey = "55RQx8xJ4d859UcH"
+const transactionKey = "55RQx8xJ4d859UcH";
 
 //Endpoint
 const endpoint = "https://apitest.authorize.net/xml/v1/request.api";
 
-
 //Function to make a HTTP Request
-var request = require('request');
+var request = require("request");
 
-
-// Route to redirect  from susbcribed products page to ordrs 
+// Route to redirect  from susbcribed products page to ordrs
 router.get("/suborder/:id", async (req, res) => {
-	let order = await OOrder.findOne({products: req.params.id})
+  let order = await OOrder.findOne({ products: req.params.id });
 
-	let orderid = order._id;
-	
-	let reurl = "/pages/orders/view/" + orderid 
+  let orderid = order._id;
 
-	res.redirect(reurl);
-})
+  let reurl = "/pages/orders/view/" + orderid;
 
+  res.redirect(reurl);
+});
 
-router.post('/api/subscribe', function(req, res) {
+router.post("/api/subscribe", function (req, res) {
+  req.checkBody("productid", "productid is required").notEmpty();
 
-	req.checkBody('productid', 'productid is required').notEmpty();
+  // check the validation object for errors
+  let errors = req.validationErrors();
 
-	// check the validation object for errors
-	let errors = req.validationErrors();
-
-	if (errors) {
-		res.json({ status: false, messages: errors });
-	} else {
-
-		if(req.body.productid === req.body.productmeta){
-			let subscribed = new Subscribed({...req.body, comboid: req.body.productid, combo: true})
-			subscribed.save().then((product) => res.status(200).json({ status: true, product: product }));
-		} else {
-		const subscribed = new Subscribed(req.body);
-		subscribed
-			.save()
-			.then((product) => res.status(200).json({ status: true, product: product }));
-	}}
+  if (errors) {
+    res.json({ status: false, messages: errors });
+  } else {
+    if (req.body.productid === req.body.productmeta) {
+      let subscribed = new Subscribed({
+        ...req.body,
+        comboid: req.body.productid,
+        combo: true,
+      });
+      subscribed
+        .save()
+        .then((product) =>
+          res.status(200).json({ status: true, product: product })
+        );
+    } else {
+      const subscribed = new Subscribed(req.body);
+      subscribed
+        .save()
+        .then((product) =>
+          res.status(200).json({ status: true, product: product })
+        );
+    }
+  }
 });
 
 //View any particular susbcribed product details
-router.get('/subscribed/edit/:id', async function(req, res) {
-	let query = req.params.id;
-	let product = await OrderedProduct.findById(query);
+router.get("/subscribed/edit/:id", async function (req, res) {
+  let query = req.params.id;
+  let product = await OrderedProduct.findById(query);
 
-	let postData = 															
-	{
-	  "ARBGetSubscriptionRequest": {
-		"merchantAuthentication": {
-		  name,
-		  transactionKey
-		},
-		//"refId": "123456",
-		"subscriptionId": product.subscriptionId,
-		"includeTransactions": true
-	  }
-	}
-	
-	
-        var clientServerOptions = {
-            uri: endpoint,
-            body: JSON.stringify(postData),
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/json'
-            }
-        }
-        request(clientServerOptions, function (error, response) {
-		   let data = JSON.parse(response.body.slice(1, -1).trim() + "}");
-		   //console.log(data.subscription.paymentSchedule.interval)
-         //  if(data.messages.message[0].code === "I00001" && data.messages.message[0].resultCode === "Ok" && data.transactionResponse.responseCode === "1"){res.send({status: true, data})} else {res.send({status: false, data})};
-		 return res.status(200).render('edit_subscribed.hbs', {
-			pagetitle: 'Edit Subscribed Products',
-			sub: product,
-			data
-		});
-		})
-    
-	
+  let postData = {
+    ARBGetSubscriptionRequest: {
+      merchantAuthentication: {
+        name,
+        transactionKey,
+      },
+      //"refId": "123456",
+      subscriptionId: product.subscriptionId,
+      includeTransactions: true,
+    },
+  };
+
+  var clientServerOptions = {
+    uri: endpoint,
+    body: JSON.stringify(postData),
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+  };
+  request(clientServerOptions, function (error, response) {
+    let data = JSON.parse(response.body.slice(1, -1).trim() + "}");
+    //console.log(data.subscription.paymentSchedule.interval)
+    //  if(data.messages.message[0].code === "I00001" && data.messages.message[0].resultCode === "Ok" && data.transactionResponse.responseCode === "1"){res.send({status: true, data})} else {res.send({status: false, data})};
+    return res.status(200).render("edit_subscribed.hbs", {
+      pagetitle: "Edit Subscribed Products",
+      sub: product,
+      data,
+    });
+  });
 });
 
 //post route for editing subscribed products
-router.post('/subscribed/edit/:id', function(req, res) {
-	let query = req.params.id;
-	// let subscribed = await Subscribed.findById(query);
-	let newData = {};
-	console.log(req.body.userid);
-	newData.userid = req.body.userid;
-	newData.productid = req.body.productid;
-	newData.productmeta = req.body.productmeta;
-	newData.duration = req.body.duration;
-	newData.quantity = req.body.quantity;
-	newData.deliverydate = req.body.deliverydate;
-	newData.skipdelivery = req.body.skipdelivery;
-	newData.orderid = req.body.orderid;
-	newData.subscribedon = req.body.subscribedon;
-	Subscribed.findOneAndUpdate({ _id: query }, newData)
-		.then(() => res.redirect('/all'))
-		.catch((err) => console.log(err));
+router.post("/subscribed/edit/:id", function (req, res) {
+  let query = req.params.id;
+  // let subscribed = await Subscribed.findById(query);
+  let newData = {};
+  console.log(req.body.userid);
+  newData.userid = req.body.userid;
+  newData.productid = req.body.productid;
+  newData.productmeta = req.body.productmeta;
+  newData.duration = req.body.duration;
+  newData.quantity = req.body.quantity;
+  newData.deliverydate = req.body.deliverydate;
+  newData.skipdelivery = req.body.skipdelivery;
+  newData.orderid = req.body.orderid;
+  newData.subscribedon = req.body.subscribedon;
+  Subscribed.findOneAndUpdate({ _id: query }, newData)
+    .then(() => res.redirect("/all"))
+    .catch((err) => console.log(err));
 });
 
 //delete subscribed product route
-router.get('/subscribed/delete/:id', async function(req, res) {
-	let query = req.params.id;
+router.get("/subscribed/delete/:id", async function (req, res) {
+  let query = req.params.id;
 
-	Subscribed.findById(query, function(err, product) {
-		Subscribed.findOneAndRemove({"_id": query}, function(err) {
-			if (err) {
-				console.log(err);
-			}
-			res.redirect('/all');
-		});
-	});
+  Subscribed.findById(query, function (err, product) {
+    Subscribed.findOneAndRemove({ _id: query }, function (err) {
+      if (err) {
+        console.log(err);
+      }
+      res.redirect("/all");
+    });
+  });
 });
 
 // router.post('/edit/:id', upload.any(), async function(req, res){
@@ -168,42 +170,59 @@ router.get('/subscribed/delete/:id', async function(req, res) {
 
 //         }})
 
-router.post('/subscribed/byuser', async function(req, res) {
-	
-	//let fetchCombos = await Subscribed.find({ "$expr": { "$eq": ["$productid", "$productmeta"] }});
-	let fetchCombos = await Subscribed.find({userid: req.body.userid, combo: true}).populate("comboid")
-	//let combos = await fetchCombos.map((el) => {let returnObj; return returnObj = {...el._doc, comboid: el.productid, combo: true}; return returnObj})
-	 //console.log(combos);
-	let fetchProducts = await Subscribed.find({userid: req.body.userid, combo : false}).populate("productid").populate("productmeta");
-	 //await Subscribed.find({ userid: req.body.userid })
-		//.select('-1')
-		//.sort({ deliverydate: 'desc' })
-		//.populate('userid')
-		//.populate('productid')
-		//.populate('productmeta')
-	let finalList = fetchCombos.concat(fetchProducts);
-		res.json({status: true, subscribed: finalList})
+router.post("/subscribed/byuser", async function (req, res) {
+  //let fetchCombos = await Subscribed.find({ "$expr": { "$eq": ["$productid", "$productmeta"] }});
+  let fetchCombos = await Subscribed.find({
+    userid: req.body.userid,
+    combo: true,
+  }).populate("comboid");
+  //let combos = await fetchCombos.map((el) => {let returnObj; return returnObj = {...el._doc, comboid: el.productid, combo: true}; return returnObj})
+  //console.log(combos);
+  let fetchProducts = await Subscribed.find({
+    userid: req.body.userid,
+    combo: false,
+  })
+    .populate("productid")
+    .populate("productmeta");
+  //await Subscribed.find({ userid: req.body.userid })
+  //.select('-1')
+  //.sort({ deliverydate: 'desc' })
+  //.populate('userid')
+  //.populate('productid')
+  //.populate('productmeta')
+  let finalList = fetchCombos.concat(fetchProducts);
+  res.json({ status: true, subscribed: finalList });
 });
 
-router.get('/all', async function(req, res) {
-	//DOMT FORGET TO ADD POPULATE ORDER IN THIS !!! IMPORTANT
-	// let subscribed = await Subscribed.find()
-	// 	.select('-1')
-	// 	.sort({ deliverydate: 'desc' })
-	// 	.populate('userid')
-	// 	.populate('productid')
-	// 	.populate('productmeta');
+router.get("/all", async function (req, res) {
+  //DOMT FORGET TO ADD POPULATE ORDER IN THIS !!! IMPORTANT
+  // let subscribed = await Subscribed.find()
+  // 	.select('-1')
+  // 	.sort({ deliverydate: 'desc' })
+  // 	.populate('userid')
+  // 	.populate('productid')
+  // 	.populate('productmeta');
 
-	
+  let subscribed = await OrderedProduct.find({ isSubscribed: true });
+  console.log(subscribed);
+  return res.status(200).render("subscribed.hbs", {
+    pageTitle: "All Subscribed Products",
+    products: subscribed,
+  });
+});
 
+router.get("/allSubscribedProducts", async function (req, res) {
+  //DOMT FORGET TO ADD POPULATE ORDER IN THIS !!! IMPORTANT
+  // let subscribed = await Subscribed.find()
+  // 	.select('-1')
+  // 	.sort({ deliverydate: 'desc' })
+  // 	.populate('userid')
+  // 	.populate('productid')
+  // 	.populate('productmeta');
 
-
-	let subscribed  = await OrderedProduct.find({isSubscribed: true}) 
-	console.log(subscribed);
-	return res.status(200).render('subscribed.hbs', {
-		pageTitle: 'All Subscribed Products',
-		products: subscribed
-	});
+  OrderedProduct.find({ isSubscribed: true })
+    .then((result) => res.status(200).json({ products: result }))
+    .catch((error) => res.status(404).json({ message: error.message }));
 });
 
 module.exports = router;
--- routes/wishlist.js
+++ routes/wishlist.js
@@ -17,126 +17,183 @@ const Wishlist = DB.Wishlist;
 // });
 
 //add item to wishlist api route
-router.post('/api/add', function (req, res) {
-    if( req.body.productid === req.body.productmeta){
-        Wishlist.find({ "userid": req.body.userid, "comboid": req.body.productid }).then((data) => check(data));
-        function check(data){
-            if(data.length > 0){
-                res.json({ status: false, error: "This Product Already Exists" });    
-            } else {
-                const wishlist = new Wishlist({
-                    combo: true,
-                    comboid: req.body.productid,
-                    userid: req.body.userid
-                }); 
-                wishlist.save().then((wishlist) => res.status(200).json({ status: true, wishlist: wishlist }))
-            }
-        }
-    } else {
-
-
+router.post("/api/add", function (req, res) {
+  if (req.body.productid === req.body.productmeta) {
+    Wishlist.find({
+      userid: req.body.userid,
+      comboid: req.body.productid,
+    }).then((data) => check(data));
+    function check(data) {
+      if (data.length > 0) {
+        res.json({ status: false, error: "This Product Already Exists" });
+      } else {
+        const wishlist = new Wishlist({
+          combo: true,
+          comboid: req.body.productid,
+          userid: req.body.userid,
+        });
+        wishlist
+          .save()
+          .then((wishlist) =>
+            res.status(200).json({ status: true, wishlist: wishlist })
+          );
+      }
+    }
+  } else {
     req.checkBody("userid", "userid is required");
     let errors = req.validationErrors();
-    Wishlist.find({ "userid": req.body.userid, "productid": req.body.productid }).then((data) => check(data));
+    Wishlist.find({
+      userid: req.body.userid,
+      productid: req.body.productid,
+    }).then((data) => check(data));
     function check(data) {
-        if (data.length > 0) {
-            res.json({ status: false, error: "This Product Already Exists" });
+      if (data.length > 0) {
+        res.json({ status: false, error: "This Product Already Exists" });
+      } else {
+        if (errors) {
+          res.json({ status: false, message: errors });
         } else {
-            if (errors) {
-                res.json({ status: false, message: errors });
-            } else {
-                const wishlist = new Wishlist(req.body);
-                wishlist.save().then((wishlist) => res.status(200).json({ status: true, wishlist: wishlist }))
-            }
+          const wishlist = new Wishlist(req.body);
+          wishlist
+            .save()
+            .then((wishlist) =>
+              res.status(200).json({ status: true, wishlist: wishlist })
+            );
         }
+      }
     }
-}});
-
-
-//delete item from wishlist api route 
-router.post('/api/delete', function(req, res){
-    req.checkBody('id', 'is is required');
-    let errors = req.validationErrors();
-
-    if(errors){
-        res.json({status: false, message: errors});
-    } else {
-        let query = req.body.id;
-        Wishlist.findOneAndRemove({_id: query}).then(() => res.status(200).json({success: true, message:"Succesfully removed"})).catch((err) => res.status(400).json({success: false, error: err}));
-    }
+  }
 });
 
+//delete item from wishlist api route
+router.post("/api/delete", function (req, res) {
+  req.checkBody("id", "is is required");
+  let errors = req.validationErrors();
+
+  if (errors) {
+    res.json({ status: false, message: errors });
+  } else {
+    let query = req.body.id;
+    Wishlist.findOneAndRemove({ _id: query })
+      .then(() =>
+        res.status(200).json({ success: true, message: "Succesfully removed" })
+      )
+      .catch((err) => res.status(400).json({ success: false, error: err }));
+  }
+});
 
 // get al the wishlist items for any particular user
-router.post('/api/byuser', function(req, res){
-    
-    // req.checkbody("userid", "Userid is required");
+router.post("/api/byuser", function (req, res) {
+  // req.checkbody("userid", "Userid is required");
 
-    // let errors = req.validationErrors();
+  // let errors = req.validationErrors();
 
-    // if(errors){
-    //     res.json({status: false, messages: errors});
-    // } else {
+  // if(errors){
+  //     res.json({status: false, messages: errors});
+  // } else {
 
-    let user = req.body.userid;
-    // Wishlist.find({userid: user}).then((wishlist) => res.status(200).json({success: true, wishlist: wishlist})).catch((err) => res.status(400).json({success: false, error: err}))
-    Wishlist.find({userid: user, combo: false}).populate("productid").populate("productmeta").then((wishlist) => display(wishlist));
-    function display(data){
-        Wishlist.find({userid: user, combo: true}).populate("comboid").then((data1) => res.status(200).json({success: true, wishlist: data, combo: data1})).catch((err) => res.status(400).json({success: false, error: err}))
-    }
+  let user = req.body.userid;
+  // Wishlist.find({userid: user}).then((wishlist) => res.status(200).json({success: true, wishlist: wishlist})).catch((err) => res.status(400).json({success: false, error: err}))
+  Wishlist.find({ userid: user, combo: false })
+    .populate("productid")
+    .populate("productmeta")
+    .then((wishlist) => display(wishlist));
+  function display(data) {
+    Wishlist.find({ userid: user, combo: true })
+      .populate("comboid")
+      .then((data1) =>
+        res.status(200).json({ success: true, wishlist: data, combo: data1 })
+      )
+      .catch((err) => res.status(400).json({ success: false, error: err }));
+  }
 });
 
 //DONT FORGET TOA DD ENSURE AUTHENTICATEED IN ALL FUNCTIONS
 //get all the wishlist items to show in backend
-router.get('/all', async function(req, res){
-  let wishlist = await Wishlist.find({combo: false}).populate('productid').populate('userid').populate('productmeta').select('-1').sort({_id: 'asc'});
-  let combos = await Wishlist.find({combo: true}).populate('comboid').populate('userid');
+router.get("/all", async function (req, res) {
+  let wishlist = await Wishlist.find({ combo: false })
+    .populate("productid")
+    .populate("userid")
+    .populate("productmeta")
+    .select("-1")
+    .sort({ _id: "asc" });
+  let combos = await Wishlist.find({ combo: true })
+    .populate("comboid")
+    .populate("userid");
   let final = wishlist.concat(combos);
-  console.log({combos})
-  console.log({"Final list": final})
-  return res.status(200).render('wishlist_all.hbs', {
-        pageTitle: 'Wishlist of Users',
-        products: final
+  console.log({ combos });
+  console.log({ "Final list": final });
+  return res.status(200).render("wishlist_all.hbs", {
+    pageTitle: "Wishlist of Users",
+    products: final,
+  });
+});
 
-    });
+router.get("/allWishlist", async function (req, res) {
+  let wishlist = await Wishlist.find({ combo: false })
+    .populate("productid")
+    .populate("userid")
+    .populate("productmeta")
+    .select("-1")
+    .sort({ _id: "asc" });
+  let combos = await Wishlist.find({ combo: true })
+    .populate("comboid")
+    .populate("userid");
+  let final = wishlist.concat(combos);
+  console.log({ combos });
+  console.log({ "Final list": final });
+  return res.status(200).json({
+    wishlists: final,
+  });
 });
 
 //edit any particular wishlist item from backend
-router.get('/edit/:id', async function(req, res){
-    let query = req.params.id;
-    let wishlist = await Wishlist.findById(query);
+router.get("/edit/:id", async function (req, res) {
+  let query = req.params.id;
+  let wishlist = await Wishlist.findById(query);
 
-    return res.status(200).render('wishlist_edit.hbs', {
-        pageTitle: 'Edit Wishlist',
-        wishlist: wishlist
-    });
+  return res.status(200).render("wishlist_edit.hbs", {
+    pageTitle: "Edit Wishlist",
+    wishlist: wishlist,
+  });
 });
 
 //Post edit route for cditing any particular wishlist
-router.post('/edit/:id', async function(req, res){
-    let query = req.params.id;
-    
-    let newWishlist = {};
-    console.log(req.body);
-    newWishlist.serial=req.body.serial;
-    newWishlist.userid=req.body.userid;
-    newWishlist.productid=req.body.productid;
-    newWishlist.productmeta=req.body.productmeta;
-    Wishlist.findOneAndUpdate({_id: query}, newWishlist).then(() => res.redirect("/wishlist/all")).catch((err) => console.log(err));
+router.post("/edit/:id", async function (req, res) {
+  let query = req.params.id;
 
+  let newWishlist = {};
+  console.log(req.body);
+  newWishlist.serial = req.body.serial;
+  newWishlist.userid = req.body.userid;
+  newWishlist.productid = req.body.productid;
+  newWishlist.productmeta = req.body.productmeta;
+  Wishlist.findOneAndUpdate({ _id: query }, newWishlist)
+    .then(() => res.redirect("/wishlist/all"))
+    .catch((err) => console.log(err));
 });
 
 //delete any particluar wishlist item from backend
-router.get('/delete/:id', async function(req, res){
-    let query = req.params.id;
+router.get("/delete/:id", async function (req, res) {
+  let query = req.params.id;
 
-    Wishlist.findOneAndRemove(query, function(err, product){
-          if(err){
-            console.log(err);
-          }
-              res.redirect('/wishlist/all');
-        });
-    });
+  Wishlist.findOneAndRemove(query, function (err, product) {
+    if (err) {
+      console.log(err);
+    }
+    res.redirect("/wishlist/all");
+  });
+});
 
+router.get("/deleteWishlist/:id", async function (req, res) {
+  let query = req.params.id;
+
+  Wishlist.findOneAndRemove(query, function (err, product) {
+    if (err) {
+      res.status(404).json({ message: "Error Deleteing Wishlist" });
+    }
+    res.status(200).json({ message: "Wishlist Deleted Successfully" });
+  });
+});
 
 module.exports = router;
